2643616941284e390e273db21c0b7f3b47a15fd7
Anthony <info@anthonyscheeres.nl>
2019-12-24 23:15:42 +0100

    .

--- src/app/foto/foto.component.ts
+++ src/app/foto/foto.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotoComponent implements OnInit {
 
+  numbers = Array(5).fill().map((x, i) => i); // [0,1,2,3,4]
+       slideIndex = 1;
+//showSlides(slideIndex);
+
+ plusSlides(n) {
+  this.showSlides(this.slideIndex += n);
+}
+
+currentSlide(n) {
+  this.showSlides(this.slideIndex = n);
+}
+
+
+
+
+showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+
+  if (n > slides.length) {
+    this.slideIndex = 1
+  }
+  if (n < 1) {
+    this.slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[this.slideIndex - 1].style.display = "block";
+}
+
+
+
   constructor() { }
 
   ngOnInit() {
+    this.showSlides(this.slideIndex);
   }
 
 }
---
+++ src/app/models/pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from 'angular2/core';
+
+@Pipe({ name: 'demoNumber' })
+export class DemoNumber implements PipeTransform {
+  transform(value, args: string[]): any {
+    let res = [];
+    for (let i = 0; i < value; i++) {
+      res.push(i);
+    }
+    return res;
+  }
+}
